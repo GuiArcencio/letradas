32993e9cf14b3796b46703f2d3aead50f39e8864
Guilherme Arcencio <guiarcencio@hotmail.com>
2022-01-13 21:32:38 -0300

    finish home's initial design

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,37 @@
 import Head from "next/head";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import styles from "../styles/Home.module.scss";
 
 export default function Home() {
     const [letterArray, setLetterArray] = useState(["", "", "", "", ""]);
+    const [letterPointer, setLetterPointer] = useState(0);
+    const contentRef = useRef(null);
 
     function keyPressed({ key }) {
         const keyUpper = key.toUpperCase();
+
+        if (keyUpper === "BACKSPACE" && letterPointer > 0) {
+            const newArray = [...letterArray];
+            newArray[letterPointer - 1] = "";
+            setLetterArray(newArray);
+            setLetterPointer(letterPointer - 1);
+        }
+        else if (keyUpper.match(/^[A-Z]$/) && letterPointer < letterArray.length) {
+            const newArray = [...letterArray];
+            newArray[letterPointer] = keyUpper;
+            setLetterArray(newArray);
+            setLetterPointer(letterPointer + 1);
+        }
     }
 
     useEffect(() => {
-        window.addEventListener("keyup", keyPressed);
-        return () => {
-            window.removeEventListener("keyup", keyPressed);
-        };
+        if (contentRef.current)
+            contentRef.current.focus();
     }, []);
 
     return (
-        <div className={styles.content}>
+        <div className={styles.content} onKeyDown={keyPressed} tabIndex={-1} ref={contentRef}>
             <Head>
                 <title>Letrados</title>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
@@ -29,17 +42,17 @@ export default function Home() {
                 <ul>
                     {letterArray.map((letter, i) => (
                         <li key={i}>
-                            {letterArray[i]}
+                            <p>{letterArray[i]}</p>
                         </li>
                     ))}
                 </ul>
-                <button>
-                    <h3>CRIAR JOGO</h3>
-                </button>
+                <div className={styles.generateButton}>
+                    <h3>GERAR JOGO</h3>
+                </div>
             </div>
 
             <div className={styles.footer}>
-                <p>Feito por <a href="https://github.com/GuiArcencio" target="_blank" rel="noreferrer">GuiArcencio</a></p>
+                <p>Criado por <a href="https://github.com/GuiArcencio" target="_blank" rel="noreferrer">GuiArcencio</a></p>
             </div>
         </div>
     );
